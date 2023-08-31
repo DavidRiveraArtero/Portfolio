7b43a3064e46e4711a554ae82c9811fd9449b637
David Rivera <davidtoy222@gmail.com>
2023-08-31 17:45:55 +0200

    portfolioV1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { HeaderPort } from './components/header/header'
+import { Home } from './components/home/home'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <HeaderPort/>
+      <main>
+        <Home/>
+        <section className='sect_aboutMe'>
+          <p>Apasionado de la programación que quiere dar sus primeros pasos en esta 
+            industria y <span>seguir creciendo</span> tanto <span>personalmente</span> como <span>profesionalmente</span>.</p>
+        </section>
+      </main>
+      
+    </>
+  )
+}
+
+export default App
