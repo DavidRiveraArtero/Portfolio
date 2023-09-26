b536cbe7e58d861ad8cbf11cce094dce0ad8816c
David Rivera <davidtoy222@gmail.com>
2023-09-26 09:58:44 +0200

    experience

--- src/components/proyects/proyects.jsx
+++ src/components/proyects/proyects.jsx
@@ -5,39 +5,39 @@ import "./proyects.css"
 export function Proyect(){
     return (
         <section className='containerProyect' id='proyect'>
-            <h1 className='sectionTitle'>PROYECTOS</h1>
-            <section className='listProyect'>
-              {
-                proyectos.proyect.map((proyect, key)=>{
-                 
-                  return (
-                    <a key={key} className='listProyectRef' href={proyect.ruta} target="_blank">
-                      
-                      <img alt='imgProyect' className='imgProyect' src={proyect.img}/>
-                      <div className='languajeProyect'>
-                        {
-                          proyect.languaje.map((lang,key2)=>{
-                            
-                            return(
-                              <img key={key2} src={lang.ruta_lang}/>
-                            )
-                          })
-                        }
-                      </div>
-                      
-                      <figcaption>
-                        <section className='title'>
-                          <h2>{proyect.name}</h2>
-                        </section>
-                        <section className='desc'>
-                          <p>{proyect.desc}</p>
-                        </section>
-                      </figcaption>
-                    </a>
-                  )
-                })
-              }  
-            </section>
-          </section> 
+          <h1 className='sectionTitle'>PROYECTOS</h1>
+          <section className='listProyect'>
+            {
+              proyectos.proyect.map((proyect, key)=>{
+                
+                return (
+                  <a key={key} className='listProyectRef' href={proyect.ruta} target="_blank">
+                    
+                    <img alt='imgProyect' className='imgProyect' src={proyect.img}/>
+                    <div className='languajeProyect'>
+                      {
+                        proyect.languaje.map((lang,key2)=>{
+                          
+                          return(
+                            <img key={key2} src={lang.ruta_lang}/>
+                          )
+                        })
+                      }
+                    </div>
+                    
+                    <figcaption>
+                      <section className='title'>
+                        <h2>{proyect.name}</h2>
+                      </section>
+                      <section className='desc'>
+                        <p>{proyect.desc}</p>
+                      </section>
+                    </figcaption>
+                  </a>
+                )
+              })
+            }  
+          </section>
+        </section> 
     )
 }
